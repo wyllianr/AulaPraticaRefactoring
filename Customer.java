d57e3c0e0905343db6ede75a2043c4dfa25ea8d1
Wyllian Ribiski <wyribiski@gmail.com>
2024-11-24 22:25:58 -0300

    Commit 8

--- Customer.java
+++ Customer.java
@@ -33,7 +33,24 @@ public class Customer {
       return result;
    }
 
-   // Método para calcular o total de cobranças
+   public String htmlStatement() {
+      Enumeration rentals = _rentals.elements();
+      String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+         // show figures for each rental
+         result += each.getMovie().getTitle() + ": " +
+                  String.valueOf(each.getCharge()) + "<BR>\n";
+      }
+
+      // add footer lines
+      result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) + "</EM><P>\n";
+      result += "On this rental you earned <EM>" +
+            String.valueOf(getTotalFrequentRenterPoints()) +
+            "</EM> frequent renter points<P>";
+      return result;
+   }
+
    private double getTotalCharge() {
       double result = 0;
       Enumeration rentals = _rentals.elements();
@@ -44,7 +61,6 @@ public class Customer {
       return result;
    }
 
-   // Método para calcular o total de pontos de fidelidade
    private int getTotalFrequentRenterPoints() {
       int result = 0;
       Enumeration rentals = _rentals.elements();
