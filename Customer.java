9852b6be6546e4c47019d900d472e8deb1fa3d7f
Wyllian Ribiski <wyribiski@gmail.com>
2024-11-24 22:07:16 -0300

    Commit 5

--- Customer.java
+++ Customer.java
@@ -23,10 +23,7 @@ public class Customer {
       Enumeration rentals = _rentals.elements();
       String result = "Rental Record for " + getName() + "\n";
       while (rentals.hasMoreElements()) {
-         double thisAmount = 0;
          Rental each = (Rental) rentals.nextElement();
-
-         thisAmount = each.getCharge();
 
          // add frequent renter points
          frequentRenterPoints++;
@@ -35,14 +32,12 @@ public class Customer {
              each.getDaysRented() > 1) frequentRenterPoints++;
 
          // show figures for this rental
-         result += "\t" + each.getMovie().getTitle() + "\t" +
-             String.valueOf(thisAmount) + "\n";
-         totalAmount += thisAmount;
+         result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+         totalAmount += each.getCharge();
       }
       // add footer lines
       result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-      result += "You earned " + String.valueOf(frequentRenterPoints) +
-              " frequent renter points";
+      result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
       return result;
    }
 }
